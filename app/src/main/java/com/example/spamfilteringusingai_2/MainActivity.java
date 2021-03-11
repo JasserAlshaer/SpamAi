a049ca884cb53587e47d3e13996bd02595d4d107
user <daliaalkhateeb99@gmail.com>
2021-03-11 16:09:24 +0200

    spam filtering using Ai(update)

--- app/src/main/java/com/example/spamfilteringusingai_2/MainActivity.java
+++ app/src/main/java/com/example/spamfilteringusingai_2/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.spamfilteringusingai_2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 
@@ -17,9 +19,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onLoginButtonPressed(View view){
-    //move to screen number 2
-        Intent move = new Intent(MainActivity.this, MainActivity2.class);
-        startActivity(move);
+        SharedPreferences sp=getSharedPreferences("spamFillteringapp", Context.MODE_PRIVATE);
+
+        String email=sp.getString("useremail","noemail");
+        if(email.equals("noemail")) {
+            //move to screen number 2
+            Intent move = new Intent(MainActivity.this, MainActivity2.class);
+            startActivity(move);
+
+        }
+
+
+       else {
+            Intent move2 = new Intent(MainActivity.this, MainActivity3.class);
+            move2.putExtra("email",email);
+            startActivity(move2);
+        }
 
 
 
--- app/src/main/java/com/example/spamfilteringusingai_2/MainActivity3.java
+++ app/src/main/java/com/example/spamfilteringusingai_2/MainActivity3.java
@@ -2,6 +2,7 @@ package com.example.spamfilteringusingai_2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,12 +14,16 @@ public class MainActivity3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        Intent getemail=getIntent();
+        String uemail=getemail.getStringExtra("email");
         EditText pass=(EditText)findViewById(R.id.password_check);
         Button button=(Button)findViewById(R.id.loginbutton);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //code for login operation 2
+                String password=pass.getText().toString();
+                //login with GmailApi
             }
         });
     }
